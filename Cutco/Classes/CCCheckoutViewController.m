7382ea02ddd2806a0f5fa8c3070515e6c044b7de
Dmytro Cheverda <dima.cheverda@gmail.com>
2014-10-31 00:12:23 +0200

    add 'Came Back' & 'New Customer' (build 103)

--- Cutco/Classes/CCCheckoutViewController.m
+++ Cutco/Classes/CCCheckoutViewController.m
@@ -17,6 +17,7 @@
 #import "CCStockViewController.h"
 #import "UIColor+CCColor.h"
 #import "UIFont+CCFont.h"
+#import "CCCheckoutSettingCell.h"
 
 @interface CCCheckoutViewController () <UITableViewDataSource, UITableViewDelegate>
 
@@ -26,10 +27,6 @@
 @property (strong, nonatomic) NSArray *items;
 @property (strong, nonatomic) MBProgressHUD *hud;
 @property (strong, nonatomic) NSMutableArray *quantity;
-@property (strong, nonatomic) UISwitch *newCustomerSwitch;
-@property (strong, nonatomic) UILabel *newCustomerLabel;
-@property (strong, nonatomic) UISwitch *cameBackSwitch;
-@property (strong, nonatomic) UILabel *cameBackLabel;
 
 @end
 
@@ -60,10 +57,6 @@
     [self.view addSubview:self.cancelButton];
     [self.view addSubview:self.confirmButton];
     [self.view addSubview:self.tableView];
-    [self.view addSubview:self.newCustomerLabel];
-    [self.view addSubview:self.newCustomerSwitch];
-    [self.view addSubview:self.cameBackLabel];
-    [self.view addSubview:self.cameBackSwitch];
 }
 
 #define kBottomButtonsHeight 44.0
@@ -71,35 +64,10 @@
 - (void)viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     
-    _newCustomerLabel.frame = CGRectMake(16.0,
-                                         0.0,
-                                         CGRectGetWidth(self.view.frame),
-                                         kBottomButtonsHeight);
-    
-    _newCustomerSwitch.frame = CGRectMake(CGRectGetWidth(self.view.frame) - 60.0,
-                                          0.0,
-                                          64.0,
-                                          kBottomButtonsHeight);
-    _newCustomerSwitch.center = CGPointMake(CGRectGetMidX(_newCustomerSwitch.frame),
-                                            CGRectGetMidY(_newCustomerLabel.frame));
-    
-    _cameBackLabel.frame = CGRectMake(16.0,
-                                      CGRectGetMaxY(_newCustomerLabel.frame),
-                                      CGRectGetWidth(self.view.frame),
-                                      kBottomButtonsHeight);
-    
-    _cameBackSwitch.frame = CGRectMake(CGRectGetWidth(self.view.frame) - 60.0,
-                                       0.0,
-                                       64.0,
-                                       kBottomButtonsHeight);
-    _cameBackSwitch.center = CGPointMake(CGRectGetMidX(_cameBackSwitch.frame),
-                                         CGRectGetMidY(_cameBackLabel.frame));
-    
-    
     _tableView.frame = CGRectMake(0.0,
-                                  CGRectGetMaxY(_cameBackLabel.frame),
+                                  0.0,
                                   CGRectGetWidth(self.view.frame),
-                                  CGRectGetHeight(self.view.frame) - kBottomButtonsHeight * 3);
+                                  CGRectGetHeight(self.view.frame) - kBottomButtonsHeight);
     
     _cancelButton.frame = CGRectMake(0.0,
                                      CGRectGetMaxY(_tableView.frame),
@@ -118,8 +86,9 @@
 - (CCCheckoutTableView *)tableView {
     if (!_tableView) {
         _tableView = [[CCCheckoutTableView alloc] initWithFrame:CGRectZero
-                                                          style:UITableViewStylePlain];
+                                                          style:UITableViewStyleGrouped];
         [_tableView registerClass:[CCCheckoutTableViewCell class] forCellReuseIdentifier:@"Cell"];
+        [_tableView registerClass:[CCCheckoutSettingCell class] forCellReuseIdentifier:@"SettingCell"];
         _tableView.dataSource = self;
         _tableView.delegate = self;
     }
@@ -154,57 +123,58 @@
     return _confirmButton;
 }
 
-- (UILabel *)newCustomerLabel {
-    if (!_newCustomerLabel) {
-        _newCustomerLabel = [[UILabel alloc] init];
-        _newCustomerLabel.numberOfLines = 0;
-        _newCustomerLabel.text = @"New customer";
-        _newCustomerLabel.font = [UIFont primaryCopyTypefaceWithSize:20.0];
-    }
-    return _newCustomerLabel;
-}
-
-- (UISwitch *)newCustomerSwitch {
-    if (!_newCustomerSwitch) {
-        _newCustomerSwitch = [[UISwitch alloc] init];
-    }
-    return _newCustomerSwitch;
-}
-
-- (UILabel *)cameBackLabel {
-    if (!_cameBackLabel) {
-        _cameBackLabel = [[UILabel alloc] init];
-        _cameBackLabel.numberOfLines = 0;
-        _cameBackLabel.text = @"Came back";
-        _cameBackLabel.font = [UIFont primaryCopyTypefaceWithSize:20.0];
-    }
-    return _cameBackLabel;
-}
-
-- (UISwitch *)cameBackSwitch {
-    if (!_cameBackSwitch) {
-        _cameBackSwitch = [[UISwitch alloc] init];
-    }
-    return _cameBackSwitch;
-}
-
 #pragma mark - Table View DataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.items.count;
+    if (section == 0) {
+        return 2;
+    } else {
+        return self.items.count;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return CGRectGetWidth(self.view.frame) / 3.0;
+    if (indexPath.section == 0) {
+        return 44.0;
+    } else {
+        return CGRectGetWidth(self.view.frame) / 3.0;
+    }
 }
 
+//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+//    if (section == 0) {
+//        return @"Settings";
+//    } else {
+//        return @"Items";
+//    }
+//}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    CCCheckoutTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    UITableViewCell *cell;
     
+    if (indexPath.section == 0) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"SettingCell"];
+        [self setupSettingCell:(CCCheckoutSettingCell *)cell forIndexPath:indexPath];
+    } else {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+        [self setupStockItemCell:(CCCheckoutTableViewCell *)cell forIndexPath:indexPath];
+    }
+    
+    return cell;
+}
+
+#pragma mark - Cell Setup Methods
+
+- (void)setupSettingCell:(CCCheckoutSettingCell *)cell forIndexPath:(NSIndexPath *)indexPath {
+    NSArray *titles = @[@"New Customer", @"Came Back"];
+    cell.settingNameLabel.text = titles[indexPath.row];
+}
+
+- (void)setupStockItemCell:(CCCheckoutTableViewCell *)cell forIndexPath:(NSIndexPath *)indexPath {
     [cell.plusButton addTarget:self
                         action:@selector(quantityButtonDidPressed:)
               forControlEvents:UIControlEventTouchUpInside];
@@ -212,7 +182,7 @@
     [cell.minusButton addTarget:self
                          action:@selector(quantityButtonDidPressed:)
                forControlEvents:UIControlEventTouchUpInside];
-
+    
     CCStockItem *item = self.items[indexPath.row];
     [item.image getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
         if (!error) {
@@ -224,8 +194,7 @@
     }];
     
     cell.quantityLabel.text = [NSString stringWithFormat:@"%@", self.quantity[indexPath.row]];
-    
-    return cell;
+
 }
 
 #pragma mark - Action Handlers
